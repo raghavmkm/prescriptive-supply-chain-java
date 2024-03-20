d9a8e13f2eeb6541e5b3c62554a4eaf6c1054045
raghavmkm <78324747+raghavmkm@users.noreply.github.com>
2024-03-19 22:43:55 -0400

    java init:

---
+++ java/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+########################################
+############# CSCI 2951-O ##############
+########################################
+
+# Update this file with instructions on how to compile your code
+javac -classpath /local/projects/cplex/CPLEX_Studio221/cplex/lib/cplex.jar ./src/solver/lp/*.java
---
+++ java/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+########################################
+############# CSCI 2951-O ##############
+########################################
+E_BADARGS=65
+if [ $# -ne 1 ]
+then
+	echo "Usage: `basename $0` <input>"
+	exit $E_BADARGS
+fi
+	
+input=$1
+# export the solver libraries into the path
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/local/projects/cplex/CPLEX_Studio221/cplex/bin/x86-64_linux
+
+# add the solver jar to the classpath and run
+java -cp /local/projects/cplex/CPLEX_Studio221/cplex/lib/cplex.jar:src solver.lp.Main $input
